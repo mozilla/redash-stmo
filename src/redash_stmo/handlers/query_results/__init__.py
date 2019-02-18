0c1ee11f910a8a13ddedd889bc82495a24ce1965
Jannis Leidel <jannis@leidel.info>
2019-02-18 17:43:38 +0100

    Minor fixes.

--- src/redash_stmo/handlers/query_results/__init__.py
+++ src/redash_stmo/handlers/query_results/__init__.py
@@ -1,27 +1,30 @@
-from redash_stmo.resources import add_resource
-from redash_stmo.handlers import extract_table_names
-
-from redash.handlers import api
 from redash.handlers.query_results import QueryResultResource
 from redash.permissions import require_access, require_permission, view_only
 from redash.handlers.base import get_object_or_404
 from redash import models
+
+from ...resources import add_resource
+from .parser import extract_table_names
 
 
 class StmoQueryResultResource(QueryResultResource):
     @require_permission('view_query')
     def get(self, query_id=None, query_result_id=None, filetype='json'):
         if query_result_id:
-            query_result = get_object_or_404(models.QueryResult.get_by_id_and_org, query_result_id, self.current_org)
+            query_result = get_object_or_404(
+                models.QueryResult.get_by_id_and_org,
+                query_result_id,
+                self.current_org,
+            )
             # Look for queries matching this result whose data source is 'Query Results'.
             if models.Query.query.join(
-                    models.DataSource,
+                models.DataSource,
             ).filter(
                 models.Query.query_hash == query_result.query_hash,
                 models.DataSource.type == 'results',
             ).first():
-
-                for table_name in extract_table_names.extract_tables(query_result.query_text):
+                table_names = extract_table_names(query_result.query_text)
+                for table_name in table_names:
                     # Look for query IDs being accessed.
                     if table_name.startswith("query_"):
                         try:
@@ -29,7 +32,9 @@ class StmoQueryResultResource(QueryResultResource):
                         except ValueError:
                             # If it's not "query_NNN" it can't affect our permissions check here.
                             continue
-                        upstream_q = models.Query.query.filter(models.Query.id == qid).first()
+                        upstream_q = models.Query.query.filter(
+                            models.Query.id == qid
+                        ).first()
                         if upstream_q is None:
                             continue
                         # If the user making this request doesn't have permission to
@@ -39,15 +44,18 @@ class StmoQueryResultResource(QueryResultResource):
 
             require_access(query_result.data_source.groups, self.current_user, view_only)
 
-        return QueryResultResource.get(self, query_id, query_result_id, filetype)
+        return super(StmoQueryResultResource, self).get(query_id, query_result_id, filetype)
 
-def handler_queryresults(app):
 
+def extension(app):
     # remove the original resource
     del app.view_functions['query_result']
-    add_resource(app, StmoQueryResultResource,
-                 '/api/query_results/<query_result_id>.<filetype>',
-                 '/api/query_results/<query_result_id>',
-                 '/api/queries/<query_id>/results.<filetype>',
-                 '/api/queries/<query_id>/results/<query_result_id>.<filetype>',
-                 endpoint='query_result')
+    add_resource(
+        app,
+        StmoQueryResultResource,
+        '/api/query_results/<query_result_id>.<filetype>',
+        '/api/query_results/<query_result_id>',
+        '/api/queries/<query_id>/results.<filetype>',
+        '/api/queries/<query_id>/results/<query_result_id>.<filetype>',
+        endpoint='query_result',
+    )
