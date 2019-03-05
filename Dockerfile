f1e5706b119c02e99a20222932189f54dc169872
Allen Short <ashort@mozilla.com>
2019-03-05 11:14:29 -0600

    Status line for Postgres and BigQuery queries
    Displays an indicator of syntax correctness or errors, updated as you type; for
    BigQuery shows estimated data usage as well.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 FROM mozilla/redash:latest
 
 USER root
-RUN apt-get update && apt-get install -y python3 python3-pip
+RUN apt-get update && apt-get install -y python3 python3-pip libecpg-dev
 RUN pip uninstall -qy redash-stmo \
 	&& pip3 install flit
 RUN mkdir -p /home/redash/.cache /home/redash/.local /app/node_modules && \
 	chown -R redash /home/redash/.cache /home/redash/.local /app/node_modules
+RUN pip install pgsanity
 USER redash
 
 ENTRYPOINT ["/extension/bin/docker-entrypoint"]
---
+++ redash_stmo/data_sources/query_status/bundle/query_status.js
@@ -0,0 +1,69 @@
+/* eslint-disable camelcase */
+import React from 'react';
+import { render } from 'react-dom';
+import { debounce } from 'lodash';
+
+function getQueryStatus($http, query, data_source_id) {
+  return $http.post('api/query_check', { query, data_source_id });
+}
+
+class QueryStatus extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getQueryStatus = debounce(getQueryStatus, 500);
+    this.state = {
+      valid: false,
+      report: '',
+    };
+  }
+  componentDidMount() {
+    this.componentDidUpdate();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const queryTextChanged = !prevProps || prevProps.queryText !== this.props.queryText;
+    const reportChanged = prevState && prevState.report !== this.state.report;
+    if (queryTextChanged || reportChanged) {
+      const p = this.getQueryStatus(this.props.$http, this.props.queryText, this.props.dataSourceId);
+      if (p) {
+        p.then((response) => { this.setState(response.data); });
+      }
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <i className={`zmdi zmdi-${this.state.valid ? 'check' : 'minus'}-circle`} /> {this.state.report}
+      </React.Fragment>
+    );
+  }
+}
+
+export default function init(ngModule) {
+  ngModule.decorator('queryEditorDirective', ['$delegate', '$http', ($delegate, $http) => {
+    const controller = $delegate[0].controller;
+    const controllerFunc = controller[controller.length - 1];
+    controllerFunc.prototype.origRender = controllerFunc.prototype.render;
+
+    controllerFunc.prototype.render = function newRender() {
+      this.origRender();
+      const container = document.querySelector('.editor__control div');
+      let status = document.querySelector('#stmo_query_status');
+      if (!status) {
+        status = document.createElement('div');
+        status.id = 'stmo_query_status';
+      } else {
+        container.removeChild(status);
+      }
+      container.appendChild(status);
+      render(
+        <QueryStatus queryText={this.props.queryText} dataSourceId={this.dataSource.id} $http={$http} />,
+        status,
+      );
+    };
+    return $delegate;
+  }]);
+}
+
+init.init = true;
